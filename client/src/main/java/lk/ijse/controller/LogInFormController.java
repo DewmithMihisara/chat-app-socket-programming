4800bda77eb6f278f06190b245caa93a101a851a
dewmithmihisara <mihisaralokuhewage@>
2023-06-28 03:50:47 +0530

    login ui done temp

--- client/src/main/java/lk/ijse/controller/LogInFormController.java
+++ client/src/main/java/lk/ijse/controller/LogInFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LogInFormController {
+    @FXML
+    private Button logInBtn;
+
+    @FXML
+    private TextField usrNameTxt;
+
+    @FXML
+    void logInBtnOnAction(ActionEvent event) {
+        logInBtn.fire();
+    }
+
+    @FXML
+    void usrNameTxtOnAction(ActionEvent event) {
+
+    }
 }
