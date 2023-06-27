033236f3bfcdd24718ce6cea85434021ed9c4605
dewmithmihisara <mihisaralokuhewage@>
2023-06-28 03:31:00 +0530

    just create ui and controllers

--- client/src/main/java/lk/ijse/Main.java
+++ client/src/main/java/lk/ijse/Main.java
@@ -1,7 +1,19 @@
 package lk.ijse;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/logInForm.fxml")));
+        stage.setTitle("LogIn");
+        stage.setScene(new Scene(parent));
+        stage.show();
     }
 }
