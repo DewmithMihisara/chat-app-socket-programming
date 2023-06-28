c80d0ea219abeef27c7e6878811de671d74a039d
dewmithmihisara <mihisaralokuhewage@>
2023-06-28 12:55:37 +0530

    validate the text field

--- client/src/main/java/lk/ijse/controller/LogInFormController.java
+++ client/src/main/java/lk/ijse/controller/LogInFormController.java
@@ -7,12 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class LogInFormController {
+    @FXML
+    private Line line;
     @FXML
     private Button logInBtn;
 
@@ -21,17 +25,32 @@ public class LogInFormController {
 
     @FXML
     void logInBtnOnAction(ActionEvent event) throws IOException {
-        Parent anchorPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/clientForm.fxml")));
-        Scene scene = new Scene(anchorPane);
+        if(usrNameTxt.getText().equals("")){
+            shakeLine();
+        }else {
+            defaultLine();
 
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.setTitle(usrNameTxt.getText());
-        stage.show();
+            Parent anchorPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/clientForm.fxml")));
+            Scene scene = new Scene(anchorPane);
+
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle(usrNameTxt.getText());
+            stage.show();
+
+            usrNameTxt.setText("");
+        }
     }
 
     @FXML
     void usrNameTxtOnAction(ActionEvent event) {
         logInBtn.fire();
+    }
+    void shakeLine(){
+        this.line.setStroke(Color.RED);
+        new animatefx.animation.Shake(this.line).play();
+    }
+    void defaultLine(){
+        this.line.setStroke(Color.BLACK);
     }
 }
